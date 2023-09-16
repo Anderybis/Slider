2d30a1a738fe31ddae3da24a6a6a9d357d920325
Anderybis <78993365+Anderybis@users.noreply.github.com>
2023-09-16 20:27:34 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
-const up = document.querySelector('.up-button')
-const down = document.querySelector('.down-button')
-const sidebar = document.querySelector('.sidebar')
-const mainSlides = document.querySelector('.main-slide')
-const divCount = mainSlides.querySelectorAll('div').length
-const container = document.querySelector('.container')
-let activeSlideIndex = 0
+const up = document.querySelector(".up-button");
+const down = document.querySelector(".down-button");
+const sidebar = document.querySelector(".sidebar");
+const mainSlides = document.querySelector(".main-slide");
+const divCount = mainSlides.querySelectorAll("div").length;
+const container = document.querySelector(".container");
+let activeSlideIndex = 0;
 
-sidebar.style.top = `-${(divCount - 1) * 100}vh`
+sidebar.style.top = `-${(divCount - 1) * 100}vh`;
 
-up.addEventListener('click', () => {
-changeSlide('up')
-})
+up.addEventListener("click", () => {
+	changeSlide("up");
+});
 
-down.addEventListener('click', () => {
-    changeSlide('down')
-})
+down.addEventListener("click", () => {
+	changeSlide("down");
+});
 
+document.addEventListener("keydown", (event) => {
+	if (event.key === "ArrowUp") changeSlide("up");
+	else if (event.key === "ArrowDown") changeSlide("down");
+});
 
 function changeSlide(direction) {
-if(direction === 'up'){
-    activeSlideIndex++
-    if(activeSlideIndex === divCount){
-        activeSlideIndex = 0
-    }
-} else if(direction === 'down'){
-    activeSlideIndex--
-    if (activeSlideIndex < 0) {
-        activeSlideIndex = divCount - 1
-    }
+	if (direction === "up") {
+		activeSlideIndex++;
+		if (activeSlideIndex === divCount) {
+			activeSlideIndex = 0;
+		}
+	} else if (direction === "down") {
+		activeSlideIndex--;
+		if (activeSlideIndex < 0) {
+			activeSlideIndex = divCount - 1;
+		}
+	}
+
+	const height = container.clientHeight;
+
+	mainSlides.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+	sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
 }
-
-const height = container.clientHeight
-
-mainSlides.style.transform = `translateY(-${activeSlideIndex * height}px)`
-sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
-
-}
-
